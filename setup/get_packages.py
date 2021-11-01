3fb7e59a73abb51cdea782cf98df54f4e6e0bfc2
Russ Poldrack <poldrack@gmail.com>
2021-11-01 11:34:01 -0700

    clean up older code

--- setup/get_packages.py
+++ setup/get_packages.py
@@ -7,7 +7,7 @@ import os,glob
 
 
 files=glob.glob('../*.Rmd') 
-packages=[]
+packages=['StanHeaders']
 for file in files:
     with open(file) as f:
         lines=[i.strip().split('(')[1].replace(')','') for i in f.readlines() if i.find('library')==0 or i.find('require')==0]
@@ -22,12 +22,8 @@ with open('dockerfile_includes','w') as f:
     for p in packages:
        f.write('"%s", \\\n'%p) 
 with open('package_installs.R','w') as f:
-    # solution to rstan compilation problem from
-    # https://discourse.mc-stan.org/t/ubuntu-20-04-rstan-installation-issues/19556/3
-    f.write('install.packages("remotes")\n')
-    f.write('deps <- remotes::package_deps("rstan", dependencies = TRUE)\n')
-    f.write('deps <- deps[!deps$package == "shinystan", ]\n')
-    f.write('update(deps)\n')    
+    # solution to rstan compilation problem 
+    f.write("install.packages('rstan', repos='https://cloud.r-project.org/', dependencies=TRUE)\n")
     for p in packages:
         f.write('if (!require("%s")) install.packages("%s",repos="https://cran.rstudio.com",dependencies=TRUE)\n'%(p,p))
     f.write('install.packages("https://cran.r-project.org/src/contrib/Archive/fiftystater/fiftystater_1.0.1.tar.gz",repos=NULL,dependencies=TRUE)\n')
